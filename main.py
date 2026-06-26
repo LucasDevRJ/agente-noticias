20b3849ded0b5a4f7b2066b5ec7669fd52ceed8b
Lucas <lucas.pereira@animati.com.br>
2026-06-26 16:52:53 -0300

    feat: integra feed RSS com resumo usando LangChain

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+from news import buscar_noticias
+
 
 parser = StrOutputParser()
+noticias = buscar_noticias()
+primeira_noticia = noticias[0]
 
 # Carrega o .env
 load_dotenv()
@@ -15,13 +19,17 @@ llm = ChatOpenAI(
 )
 
 # Prompt
-prompt = ChatPromptTemplate.from_template(
-    """
-    Explique o seguinte assunto como se eu tivesse 10 anos:
+prompt = ChatPromptTemplate.from_template("""
+    Você é um jornalista.
 
-    {assunto}
-    """
-)
+    Faça um resumo da notícia abaixo em no máximo 3 frases.
+
+    Título:
+    {titulo}
+
+    Conteúdo:
+    {conteudo}
+""")
 
 # Cria a Chain
 chain = prompt | llm | parser
@@ -29,43 +37,10 @@ chain = prompt | llm | parser
 # Executa
 resposta = chain.invoke(
     {
-        "assunto": "Agentes de IA"
+        "titulo": primeira_noticia.title,
+        "conteudo": primeira_noticia.summary
     }
 )
 
 # Mostra a resposta
 print(resposta)
-
-# Importa função
-from news import buscar_noticias
-
-# Cria variável
-noticias = buscar_noticias()
-
-# Exibe valor da variável
-print(noticias)
-
-# Especifica o tipo da variável
-print(type(noticias))
-
-# Exibe o tamanho da variável
-print(len(noticias))
-
-# Exibe a primeira notícia do vetor Noticias
-print(noticias[0])
-
-# Exibe o título da Notícia
-print(noticias[0].title)
-
-# Exibe a URL
-print(noticias[0].link)
-
-# Data da publicação
-print(noticias[0].published)
-
-# Sumario da notícia
-print(noticias[0].summary)
-
-# Exibe títulos das notícias
-for noticia in noticias:
-    print(noticia.title)
