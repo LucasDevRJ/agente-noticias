4f1e64eb8b0cdb195957a8d3569ad1586e6bbc8f
Lucas <lucaspereiradelima2020@gmail.com>
2026-06-27 10:38:37 -0300

    refactor: reorganiza projeto e corrige configuração do ambiente

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from news import buscar_noticias
 from summarizer import resumir_noticia
 
+resumos = []
 noticias = buscar_noticias()
 
 for noticia in noticias[:5]:
-    print("Exibindo noticia:")
-    print(resumir_noticia(noticia))
+    resumos.append(
+        {
+            "titulo": noticia.title,
+            "resumo": resumir_noticia(noticia),
+            "link": noticia.link
+        }
+    )
+    
+print(resumos)
